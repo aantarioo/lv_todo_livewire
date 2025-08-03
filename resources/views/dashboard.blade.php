292033f31b8f1540bb36bc593ab0c304e8ec7651
aantarioo <nazar.loboda3@mail.ru>
2025-08-03 18:53:38 +0300

    some changes, check count($tasks) in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,11 @@
 <x-layouts.app :title="__('My Tasks')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+			@if (count($tasks) === 0)
+						<div class="w-[50px]">
+						<flux:heading size="xl">Add a new task to the list</flux:heading>
+						<a href="{{ route('newtask') }}" class="mt-3"><flux:button icon="plus" class="mt-3">New Task</flux:button></a>
+						</div>		
+			@endif
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
 						@foreach($tasks as $task)
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
@@ -7,23 +13,12 @@
 									<flux:separator text="{{ $task->created_at->format('d.m.Y') }}" />
         					<flux:heading size="xl" class="mb-2">{{ $task->task_name }}</flux:heading>
     							<flux:subheading class="mb-3">{{ $task->task_description }}</flux:subheading>
-								<div class="flex mt-3">
-									<flux:modal.trigger name="edit-task">
+									<div class="flex mt-3">
+									<flux:modal.trigger name="edit-task-{{ $task->id }}">
 									<flux:button variant="primary" size="sm">Edit Task</flux:button>
 									</flux:modal.trigger>
-									<form action="/dashboard/{{ $task->id }}" method="POST">
-									{{ csrf_field() }}
-           				{{ method_field('DELETE') }}
-    							<flux:button variant="danger" type="submit" size="sm" class="ml-3">Delete Task</flux:button>
-									</form>
-								</div>
-								</div>
-            </div>
-						@endforeach					
-        </div>
-    </div>
 
-<flux:modal name="edit-task" class="md:w-96" variant="flyout">
+									<flux:modal name="edit-task-{{ $task->id }}" class="md:w-96" variant="flyout">
 <form action="/dashboard/{{ $task->id }}" method="POST">
 {{ csrf_field() }}
 {{ method_field('PUT') }}
@@ -41,5 +36,18 @@
     </div>
 </form>
 </flux:modal>
+
+									<form action="/dashboard/{{ $task->id }}" method="POST">
+									{{ csrf_field() }}
+           				{{ method_field('DELETE') }}
+    							<flux:button variant="danger" type="submit" size="sm" class="ml-3">Delete Task</flux:button>
+									</form>
+								</div>
+								</div>
+            </div>
+						@endforeach					
+        </div>
+    </div>
+
 
 </x-layouts.app>
